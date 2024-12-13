8e33b7648497ffeeca1482e652b088ed69d1e0a4
Sebastian Weber <s-weber85@gmx.de>
2024-12-13 14:20:51 +0100

    add TailwindCSS and PostCSS configuration with initial styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,9 @@ function App() {
   return (
     <>
       <h1>TEMPLATE-business-and-services</h1>
-
+      <h1 className="text-3xl font-bold underline">
+      Hello TailwindCSS world!
+    </h1>
     </>
   )
 }
