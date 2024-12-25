49a30a505b8601b0f0376a3230b8f47b464f489f
Sebastian Weber <s-weber85@gmx.de>
2024-12-25 20:13:43 +0100

    update Section_Impressum component with detailed legal information and improved formatting

--- frontend/src/components/Section_Impressum.tsx
+++ frontend/src/components/Section_Impressum.tsx
@@ -3,18 +3,39 @@
 function Section_Impressum() {
   return (
     <>
-    <div>
+    <div className="my-44">
+        <h1>Impressum</h1>
+        <br></br>
+        <ol>
+            <li>
+                <h2>Angaben gemäß § 5 TMG</h2>
+                <p>[Name des Unternehmens oder Betreibers der Website][Vertretungsberechtigter, z. B. Geschäftsführer][Adresse][PLZ Ort]Telefon: [Telefonnummer]E-Mail: [E-Mail-Adresse]</p>
+                <br></br>
+                <h2>Registereintrag:</h2>
+                <p>Eintragung im Handelsregister.</p>
+                <p>Registergericht: [Name des Registergerichts]Registernummer: [Handelsregisternummer] Umsatzsteuer-ID:Umsatzsteuer-Identifikationsnummer gemäß § 27 a Umsatzsteuergesetz: [USt-ID]</p>
+                <p>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV:[Name, Adresse]</p>
+                <p>Streitschlichtung:Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
+            </li>
+        </ol>
+        <br></br>
+        <hr></hr>
+        <br></br>
+        <br></br>
+        <br></br>
         <h1>Datenschutzerklärung</h1>
         <br></br>
         <ol>
             <li>
                 <h2>1. Allgemeine Hinweise</h2>
+                <br></br>
                 <p>Wir nehmen den Schutz Ihrer persönlichen Daten sehr ernst. Ihre personenbezogenen Daten behandeln wir vertraulich und entsprechend der gesetzlichen Datenschutzvorschriften sowie dieser Datenschutzerklärung.</p>
                 <br></br>
             </li>
 
             <li>
                 <h2>2. Verantwortlicher für die Datenverarbeitung</h2>
+                <br></br>
                 <p>[Name des Unternehmens oder Betreibers der Website][Adresse][PLZ Ort]Telefon: [Telefonnummer]E-Mail: [E-Mail-Adresse]</p>
                 <br></br>
             </li>
@@ -23,7 +44,9 @@ function Section_Impressum() {
                 <h2>3. Erhebung und Speicherung personenbezogener Daten sowie Art und Zweck der Verwendung</h2>
                 <ul>
                     <li>
+                    <br></br>
                     <p><b>Beim Besuch der Website:</b></p>
+                    <br></br>
                     Beim Aufrufen unserer Website werden durch den auf Ihrem Endgerät zum Einsatz kommenden Browser automatisch Informationen an den Server unserer Website gesendet. Diese Informationen werden temporär in einem sog. Logfile gespeichert. Die folgenden Informationen werden dabei ohne Ihr Zutun erfasst und bis zur automatisierten Löschung gespeichert:
                     </li>
                     <li>IP-Adresse,
@@ -54,7 +77,9 @@ function Section_Impressum() {
                     <br></br>
 
                     <li>
-                        <p>Bei der Nutzung unseres Kontaktformulars: Bei Fragen jeglicher Art bieten wir Ihnen die Möglichkeit, mit uns über ein auf der Website bereitgestelltes Formular Kontakt aufzunehmen. Dabei ist die Angabe einer gültigen E-Mail-Adresse erforderlich, damit wir wissen, von wem die Anfrage stammt und um diese beantworten zu können. Weitere Angaben können freiwillig getätigt werden. Die Datenverarbeitung zum Zwecke der Kontaktaufnahme mit uns erfolgt nach Art. 6 Abs. 1 S. 1 lit. a DSGVO auf Grundlage Ihrer freiwillig erteilten Einwilligung.</p>
+                        <p><b>Bei der Nutzung unseres Kontaktformulars:</b></p>
+                        <br></br>
+                        <p>Bei Fragen jeglicher Art bieten wir Ihnen die Möglichkeit, mit uns über ein auf der Website bereitgestelltes Formular Kontakt aufzunehmen. Dabei ist die Angabe einer gültigen E-Mail-Adresse erforderlich, damit wir wissen, von wem die Anfrage stammt und um diese beantworten zu können. Weitere Angaben können freiwillig getätigt werden. Die Datenverarbeitung zum Zwecke der Kontaktaufnahme mit uns erfolgt nach Art. 6 Abs. 1 S. 1 lit. a DSGVO auf Grundlage Ihrer freiwillig erteilten Einwilligung.</p>
                     </li>
                 </ul>
                 <br></br>
@@ -62,47 +87,71 @@ function Section_Impressum() {
 
             <li>
                 <h2>4. Weitergabe von Daten</h2>
-                <p>....</p>
+                <br></br>
+                <p>Eine Übermittlung Ihrer persönlichen Daten an Dritte zu anderen als den im Folgenden aufgeführten Zwecken findet nicht statt. Wir geben Ihre persönlichen Daten nur an Dritte weiter, wenn:</p>
+                <br></br>
+                <ul>
+                    <li>
+                        Sie Ihre ausdrückliche Einwilligung dazu erteilt haben (Art. 6 Abs. 1 S. 1 lit. a DSGVO),
+                    </li>
+                    <li>
+                        die Weitergabe nach Art. 6 Abs. 1 S. 1 lit. f DSGVO zur Geltendmachung, Ausübung oder Verteidigung von Rechtsansprüchen erforderlich ist und kein Grund zur Annahme besteht, dass Sie ein überwiegendes schutzwürdiges Interesse an der Nichtweitergabe Ihrer Daten haben,
+                    </li>
+                    <li>
+                        für den Fall, dass für die Weitergabe nach Art. 6 Abs. 1 S. 1 lit. c DSGVO eine gesetzliche Verpflichtung besteht, sowie
+                    </li>
+                    <li>
+                        dies gesetzlich zulässig und nach Art. 6 Abs. 1 S. 1 lit. b DSGVO für die Abwicklung von Vertragsverhältnissen mit Ihnen erforderlich ist.
+                    </li>
+                </ul>
                 <br></br>
             </li>
 
             <li>
                 <h2>5. Betroffenenrechte</h2>
-                <p>...</p>
                 <br></br>
+                <p>Sie haben das Recht:</p>
+                <br></br>
+                <ul>
+                    <li>
+                        gemäß Art. 15 DSGVO Auskunft über Ihre von uns verarbeiteten personenbezogenen Daten zu verlangen,
+                    </li>
+                    <li>
+                        gemäß Art. 16 DSGVO unverzüglich die Berichtigung unrichtiger oder Vervollständigung Ihrer bei uns gespeicherten personenbezogenen Daten zu verlangen,
+                    </li>
+                    <li>
+                        gemäß Art. 17 DSGVO die Löschung Ihrer bei uns gespeicherten personenbezogenen Daten zu verlangen,
+                    </li>
+                    <li>
+                        gemäß Art. 18 DSGVO die Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu verlangen,
+                    </li>
+                    <li>
+                        gemäß Art. 20 DSGVO Ihre personenbezogenen Daten in einem strukturierten, gängigen und maschinenlesbaren Format zu erhalten oder die Übermittlung an einen anderen Verantwortlichen zu verlangen,
+                    </li>
+                    <li>
+                        gemäß Art. 7 Abs. 3 DSGVO Ihre einmal erteilte Einwilligung jederzeit gegenüber uns zu widerrufen und
+                    </li>
+                    <li>
+                        gemäß Art. 77 DSGVO sich bei einer Aufsichtsbehörde zu beschweren.
+                    </li>
+                </ul>
             </li>
 
             <li>
+            <br></br>
                 <h2>6. Datensicherheit</h2>
-                <p>...</p>
+                <br></br>
+                <p>Wir verwenden innerhalb des Website-Besuchs das verbreitete SSL-Verfahren (Secure Socket Layer) in Verbindung mit der jeweils höchsten Verschlüsselungsstufe, die von Ihrem Browser unterstützt wird.</p>
                 <br></br>
             </li>
 
             <li>
                 <h2>7. Aktualität und Änderung dieser Datenschutzerklärung</h2>
-                <p>...</p>
+                <br></br>
+                <p>Diese Datenschutzerklärung ist aktuell gültig und hat den Stand [Datum].</p>
                 <br></br>
             </li>
             </ol>
-            <hr></hr>
-            <br></br>
-            <h1>Impressum</h1>
-            <br></br>
-            <ol>
-            <li>
-                <h2>Angaben gemäß § 5 TMG</h2>
-                <p>[Name des Unternehmens oder Betreibers der Website][Vertretungsberechtigter, z. B. Geschäftsführer][Adresse][PLZ Ort]Telefon: [Telefonnummer]E-Mail: [E-Mail-Adresse]</p>
-                <br></br>
-                <h2>Registereintrag:</h2>
-                <p>Eintragung im Handelsregister.</p>
-                <p>Registergericht: [Name des Registergerichts]Registernummer: [Handelsregisternummer] Umsatzsteuer-ID:Umsatzsteuer-Identifikationsnummer gemäß § 27 a Umsatzsteuergesetz: [USt-ID]</p>
-                <p>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV:[Name, Adresse]</p>
-                <p>Streitschlichtung:Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
-            </li>
-            </ol>
-
-
-
 
     </div>
     </>
