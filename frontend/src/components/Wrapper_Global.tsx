8932de731518696fdc2e1ce3c3ec7eeeb5eabe29
Sebastian Weber <s-weber85@gmx.de>
2025-01-03 13:17:52 +0100

    remove unused Firmenlogo SVG and clean up Footer and Navbar components

--- frontend/src/components/Wrapper_Global.tsx
+++ frontend/src/components/Wrapper_Global.tsx
@@ -1,32 +1,32 @@
 
 
-import Header from './Header';
-import SectionHero from './Section_Hero';
-import SectionTextAndImage from './Section_Text_and_Image';
-// import SectionCards from './Section_Text_and_Cards';
-import Section_Gallery from './Section_Gallery';
-import Footer from './Footer';
+// import Header from './Header';
+// import SectionHero from './Section_Hero';
+// import SectionTextAndImage from './Section_Text_and_Image';
+// // import SectionCards from './Section_Text_and_Cards';
+// import Section_Gallery from './Section_Gallery';
+// import Footer from './Footer';
 
 
-function WrapperGlobal() {
-  return (
-    <div className="w-full max-w-screen-2xl mx-auto px-4">
-        <Header></Header>
-        <SectionHero paragraph="Your paragraph text" image="your-image-url.jpg" background="your-background-color"></SectionHero>
-        <SectionTextAndImage paragraph="Your paragraph text" image="your-image-url.jpg" heading="Your heading text"></SectionTextAndImage>
-        {/* <SectionCards></SectionCards> */}
-        <Section_Gallery 
-            image1="your-image1-url.jpg" 
-            image2="your-image2-url.jpg" 
-            image3="your-image3-url.jpg" 
-            image4="your-image4-url.jpg" 
-            image5="your-image5-url.jpg" 
-            image6="your-image6-url.jpg"
-            image7="your-image7-url.jpg"
-        ></Section_Gallery>
-        <Footer></Footer>
-    </div>
-  );
-}
+// function WrapperGlobal() {
+//   return (
+//     <div className="w-full max-w-screen-2xl mx-auto px-4">
+//         <Header></Header>
+//         <SectionHero paragraph="Your paragraph text" image="your-image-url.jpg" background="your-background-color"></SectionHero>
+//         <SectionTextAndImage paragraph="Your paragraph text" image="your-image-url.jpg" heading="Your heading text"></SectionTextAndImage>
+//         <SectionCards></SectionCards>
+//         <Section_Gallery 
+//             image1="your-image1-url.jpg" 
+//             image2="your-image2-url.jpg" 
+//             image3="your-image3-url.jpg" 
+//             image4="your-image4-url.jpg" 
+//             image5="your-image5-url.jpg" 
+//             image6="your-image6-url.jpg"
+//             image7="your-image7-url.jpg"
+//         ></Section_Gallery>
+//         <Footer></Footer>
+//     </div>
+//   );
+// }
 
-export default WrapperGlobal
+// export default WrapperGlobal
