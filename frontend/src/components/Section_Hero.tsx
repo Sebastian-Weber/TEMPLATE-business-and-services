5e3875ade30d2266a24b7d642906e502ae0eb87b
Sebastian Weber <s-weber85@gmx.de>
2025-02-02 17:47:12 +0100

    refactor: enhance SectionHero component layout and adjust h4 font sizes for better responsiveness

--- frontend/src/components/Section_Hero.tsx
+++ frontend/src/components/Section_Hero.tsx
@@ -14,12 +14,12 @@ interface SectionHeroProps {
 function SectionHero({ heading, subheading, paragraph, image, logo, background = cdColors.tertiaryColor }: SectionHeroProps) {
   return (
     <>
-      <div className='min-w-full' style={{ backgroundColor: background }}>
+      <div className='min-w-full max-h-screen' style={{ backgroundColor: background }}>
 
-      <div className="mt-10 min-h-full flex flex-col h-screen w-fit justify-around items-center md:min-w-full md:flex-row">
+      <div className=" min-h-full flex flex-col h-screen w-fit justify-around items-center md:min-w-full md:flex-row">
           
-          <div className="flex flex-col mt-24 p-20 justify-center items-center h-1/2 w-full md:h-fit md:w-11/12 md:items-center">
-          <div className="flex flex-col h-auto justify-center">
+          <div className="flex flex-col mt-24 p-20 justify-center items-center h-1/2 w-full md:h-fit md:w-11/12 md:items-center lg:mx-24">
+          <div className="flex flex-col pb-12 h-auto justify-center">
               {logo && (
                 <>
                   <img className='object-cover' src={logo} alt="Placeholder"></img>
